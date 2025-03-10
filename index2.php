68705ec06c09bdd684560ef394432e45237b7dfa
nartaaaa <Nartahoxha@outlook.com>
2025-03-10 17:40:22 +0100

    ...

---
+++ index2.php
@@ -0,0 +1,32 @@
+<?php
+// $subjects=array('Biology','Maths','Geography','PE','<br>');
+// echo $subjects[0];
+// echo end($subjects);
+// echo count($subjects);
+
+// for ($i=0; $i<4; $i++){
+// 	echo $subjects[$i],"<br>";
+// }
+// $len=count($subjects);
+
+// for($i=0; $i<$len; $i++){
+// 	echo $subjects[$i],"<br>";
+// }
+// array_push($subjects,'English');
+// var_dump($subjects);
+
+// array_pop($subjects);
+// var_dump($subjects);
+
+// array_shift($subjects);
+// var_dump($subjects);
+
+// echo "<br>";
+
+// array_unshift($subjects, 'English');
+// var_dump($subjects);
+
+$vlera=array(18, 19, 7 , 21, 13, 28, 31);
+var_dump(array_sum($vlera)/7);
+
+?>
